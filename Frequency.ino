cad7d5217e813f41cdcf78f28fa65a918e661202
Muhammad Taha <42276750+taha842@users.noreply.github.com>
2020-03-29 03:06:59 +0500

    Add files via upload

---
+++ Frequency.ino
@@ -0,0 +1,132 @@
+/*
+  Analog Input
+  Demonstrates analog input by reading an analog sensor on analog pin 0 and
+  turning on and off a light emitting diode(LED)  connected to digital pin 13.
+  The amount of time the LED will be on and off depends on
+  the value obtained by analogRead().
+
+  The circuit:
+  Potentiometer attached to analog input 0
+  center pin of the potentiometer to the analog pin
+  one side pin (either one) to ground
+  the other side pin to +5V
+  LED anode (long leg) attached to digital output 13
+  LED cathode (short leg) attached to ground
+
+  Note: because most Arduinos have a built-in LED attached
+  to pin 13 on the board, the LED is optional.
+
+
+  Created by David Cuartielles
+  Modified 16 Jun 2009
+  By Tom Igoe
+
+  http://arduino.cc/en/Tutorial/AnalogInput
+
+*/
+#include <math.h>
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+#include "WProgram.h"
+void setup();
+void loop();
+LiquidCrystal lcd(3, 4, 8, 9, 10, 11);
+#define acosf   acos
+int sensorPin = 0;    // select the input pin for the potentiometer
+// select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int x = 0;
+int i;
+int y;
+int z;
+long a;
+int b;
+int k = 0;
+long c;
+int arr[250] ;
+unsigned long time;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  Serial.begin(9600);
+  time = millis();
+  Serial.println(time);
+
+  for (i = 0; i < 251; i = i + 1)
+  {
+    arr[i] = analogRead(sensorPin);
+  }
+
+  time = millis();
+  Serial.println(time);
+
+  // Print a message to the LCD.
+
+
+  //for(i=0;i<251;i=i+1)
+  //{
+  //Serial.println(arr[i]);
+  //}
+
+  float f;
+
+
+  for (i = 0; i < 251; i = i + 1)
+  {
+    if (arr[i] == 0 && arr[i + 1] != 0)
+    {
+      while (arr[i + 1] != 0)
+      {
+        k = k + 1;
+        //Serial.println("k");
+        i = i + 1;
+      }
+      break;
+    }
+
+  }
+  f = 1000 / (2 * k * 0.112);
+  Serial.println(k);
+  Serial.println("FREQUENCY MEASURED IS");
+  Serial.println(f);
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Frequency is :");
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(f);
+
+}
+
+void loop() {
+
+  // read the value from the sensor:
+
+  // turn the ledPin on
+  //digitalWrite(ledPin, HIGH);
+  // stop the program for <sensorValue> milliseconds:
+
+  // turn the ledPin off:
+  //digitalWrite(ledPin, LOW);
+  // stop the program for for <sensorValue> milliseconds:
+  //delay(sensorValue);
+}
+
+int main(void)
+{
+  init();
+
+  setup();
+
+  for (;;)
+    loop();
+
+  return 0;
+}
+
+
---
+++ Protection_Relay.ino
@@ -0,0 +1,145 @@
+/*******Over Current Relay (OCR)*******
+   Author @ Engr. Muhammad Taha
+   Contact : +92-334-3992626
+   *******Projexel**********
+*/
+
+#include <Wire.h>                         // Include Wire Library
+#include <SPI.h>                          // Include SPI Protocol Library
+#include <LiquidCrystal_I2C.h>            // Include LCD Library
+#include "EmonLib.h"                      // Include Current Sensor Library                      
+
+EnergyMonitor emon1;                     // Initiliaze Current Sensor
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // Initiliaze LCD
+
+float Gen_Current ;                    // Declare Generator Current Variable
+float OverCurrent = 6.0;              // Declare Generator OverCurrent Variable (You Can Change Over Current Limit Here)
+float CutoffCurrent = 8.0;            // Declare Generator UnderCurrent Variable(You Can Change Short Circuit Limit Here)
+float RefCurrent = 0.099;               // Declare Generator Refrence Current Variable ( You Can Chnage Refrence Current Here)
+
+const int RelayPin = 2;                // Declare Relay Pin
+bool RelayStatus;                      // Relay Status (High / Low)
+
+void setup()                           // Setup Loop Which Run Only Once When Controller Will Start
+{
+  SPI.begin();
+  lcd.begin(20, 4);                    // Initiliaze LCD Communication
+  Serial.begin(9600);                  // Initiliaze Serial Monitor Communication
+  emon1.current(1, 16.0);              // Current: input pin, calibration.
+  pinMode(RelayPin, OUTPUT);
+  digitalWrite(RelayPin, LOW);
+  Get_Current();
+  lcd.clear();                        // LCD Clear
+  lcd.setCursor(0, 0);                // Set Lcd Cursor (Position)
+  lcd.print("   NED UNIVERSITY   ");  // Print This On LCD
+  lcd.setCursor(0, 2);                // Set Lcd Cursor (Position)
+  lcd.print("Loading.");              // Print This On LCD
+  delay(800);
+  lcd.setCursor(8, 2);                // Set Lcd Cursor (Position)
+  lcd.print(".");                     // Print This On
+  delay(800);
+  lcd.setCursor(9, 2);                // Set Lcd Cursor (Position)
+  lcd.print(".");                     // Print This On
+  delay(800);
+  lcd.setCursor(10, 2);               // Set Lcd Cursor (Position)
+  lcd.print(".");                     // Print This On
+  delay(800);
+  lcd.setCursor(8, 2);                // Set Lcd Cursor (Position)
+  lcd.print(".");                     // Print This On
+  lcd.clear();                          // LCD Clear
+
+}
+
+void loop()                             // Loop Which Run in Controller again & again
+{
+  Get_Current();                        // Call Get Current Function to Measure Current
+  lcd.setCursor(0, 0);                  // Set Lcd Cursor (Position)
+  lcd.print("   NED UNIVERSITY   ");    // Print This On LCD
+
+  if ( (Gen_Current > RefCurrent ) && (Gen_Current < OverCurrent) && (Gen_Current < CutoffCurrent) ) // Stable condition where current in between Over and Cutoff limits
+  {
+    lcd.setCursor(0, 1);
+    lcd.print("Current: ");
+    lcd.print(Gen_Current);
+  }
+
+  else if ((Gen_Current > RefCurrent ) && (Gen_Current > OverCurrent) && (Gen_Current < CutoffCurrent )) //Over current condition
+  {
+    lcd.setCursor(0, 1);
+    lcd.print("Current: ");
+    lcd.print(Gen_Current);
+    lcd.setCursor(0, 2);
+    lcd.print(" Over Current Fault ");
+    delay(2500);
+    Get_Current();
+    if ((Gen_Current > RefCurrent ) && (Gen_Current > OverCurrent) && (Gen_Current < CutoffCurrent ))
+    {
+      digitalWrite(RelayPin, HIGH);
+      while (1)
+      { lcd.setCursor(0, 1);
+        lcd.print("Current: ");
+        lcd.print(Gen_Current);
+        lcd.setCursor(0, 2);
+        lcd.print(" Over Current Trip ");
+        lcd.setCursor(0, 3);
+        lcd.print(" Fault Occur ");
+        delay(500);
+      }
+    }
+    lcd.clear();                          // LCD Clear
+  }
+
+  else if ((Gen_Current > RefCurrent ) && (Gen_Current > OverCurrent ) && (Gen_Current > CutoffCurrent)) // Short Circuit Current Condition
+  {
+    lcd.setCursor(0, 1);
+    lcd.print("Current: ");
+    lcd.print(Gen_Current);
+    lcd.setCursor(0, 2);
+    lcd.print(" Short Circuit Fault ");
+    delay(2500);
+    Get_Current();
+    if ((Gen_Current > CutoffCurrent) || (Gen_Current > OverCurrent))
+    {
+      digitalWrite(RelayPin, HIGH);
+      while (1)
+      { lcd.setCursor(0, 1);
+        lcd.print("Current: ");
+        lcd.print(Gen_Current);
+        lcd.setCursor(0, 2);
+        lcd.print(" Short Circuit Trip ");
+        lcd.setCursor(0, 3);
+        lcd.print(" Fault Occur ");
+        delay(500);
+      }
+    }
+    lcd.clear();                          // LCD Clear
+  }
+  else if (Gen_Current < RefCurrent )
+  {
+    lcd.setCursor(0, 1);
+    lcd.print("Current: ");
+    lcd.print("0.00");
+  }
+  else
+  {
+    lcd.setCursor(0, 1);
+    lcd.print("Current: ");
+    lcd.print("0.00");
+  }
+
+  RelayStatus = digitalRead(RelayPin);
+  if (RelayStatus == HIGH)
+  {
+    while (1) { }
+  }
+}
+
+void Get_Current()  // Current measuring function
+{
+  double Irms1 = emon1.calcIrms(5588);  // Calculate Irms only
+  Gen_Current = Irms1;                 // Store Irms value in Gen_Current Variable
+  Serial.print("Current: ");          // Current: print on serial montior
+  Serial.println(Irms1, 4);           // Irms Value print on serial montior
+}
+
+
